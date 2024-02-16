43a948ddeda80ef84205cc9e59e9f9403498db02
Aryan-Gupta2003 <aryang.gzb@gmail.com>
2024-02-16 17:21:50 +0530

    Add new class

--- least_unique_no.cpp
+++ least_unique_no.cpp
@@ -9,10 +9,10 @@
 using namespace std;
 
 // Sol for Leetcode
-class Solution
+class Solution1
 {
 public:
-    int findLeastNumOfUniqueInts(vector<int> &arr, int k)
+    int findLeastNumOfUniqueInts1(vector<int> &arr, int k)
     {
         set<int> s(arr.begin(), arr.end());
 
@@ -54,20 +54,57 @@ public:
     }
 };
 
+class Solution2
+{
+public:
+    int findLeastNumOfUniqueInts2(vector<int> &arr, int k)
+    {
+        unordered_map<int, int> map;
+        for (int i : arr)
+        {
+            map[i]++;
+        }
+
+        int n = arr.size();
+        vector<int> c(n + 1, 0);
+        for (auto it : map)
+        {
+            c[it.second]++;
+        }
+
+        int r = map.size();
+        for (int i = 1; i <= n; i++)
+        {
+            int num = min(k / i, c[i]);
+            k -= (i * num);
+            r -= num;
+            if (k < i)
+            {
+                return r;
+            }
+        }
+        return 0;
+    }
+};
+
 // For VSCode
 int main()
 {
-    Solution o1;
+    Solution1 o1;
+    Solution2 o2;
     vector<int> arr{5, 5, 4};
-    int k = 1;
-    int result = o1.findLeastNumOfUniqueInts(arr, k);
+    int k1 = 1, k2 = 3;
+    int result1 = o1.findLeastNumOfUniqueInts1(arr, k1);
+    int result2 = o2.findLeastNumOfUniqueInts2(arr, k2);
     cout << "Array : ";
     for (auto x : arr)
     {
         cout << x << " ";
     }
     cout << endl;
-    cout << "Least no of unique elements after " << k
-         << " removals : " << result << endl;
+    cout << "Least no of unique elements after " << k1
+         << " removals : " << result1 << endl;
+    cout << "Least no of unique elements after " << k2
+         << " removals : " << result2 << endl;
     return 0;
 }
