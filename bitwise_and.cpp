bb1de4f1b62de61b5478802461435a50a1971f70
Aryan-Gupta2003 <aryang.gzb@gmail.com>
2024-02-21 20:46:31 +0530

    Updated a file

--- bitwise_and.cpp
+++ bitwise_and.cpp
@@ -9,10 +9,10 @@
 using namespace std;
 
 // Sol For Leetcode
-class Solution
+class Solution1
 {
 public:
-    int rangeBitwiseAnd(int left, int right)
+    int rangeBitwiseAnd1(int left, int right)
     {
         int r = left;
         if (left == right)
@@ -27,14 +27,53 @@ public:
     }
 };
 
+class Solution2
+{
+public:
+    int rangeBitwiseAnd2(int left, int right)
+    {
+        int s = 0;
+
+        // Find the common high-order bits of left and right
+        while (left < right)
+        {
+            left >>= 1;
+            right >>= 1;
+            s++;
+        }
+
+        // Shift back the common high-order bits to their original position
+        return left << s;
+    }
+};
+
+class Solution3
+{
+public:
+    int rangeBitwiseAnd3(int left, int right)
+    {
+        while (right > left)
+        {
+            right = (right & (right - 1));
+        }
+        return right;
+    }
+};
+
 // For VSCode
 int main()
 {
-    Solution o1;
+    Solution1 o1;
+    Solution2 o2;
+    Solution3 o3;
     int left, right;
     cin >> left >> right;
-    int result = o1.rangeBitwiseAnd(left, right);
+    int result1 = o1.rangeBitwiseAnd1(left, right);
+    int result2 = o2.rangeBitwiseAnd2(left, right);
+    int result3 = o3.rangeBitwiseAnd3(left, right);
     cout << "Range : [" << left << " , " << right << "]" << endl;
-    cout << "Bitwise And Result : " << result << endl;
+    cout << "Bitwise And Result with class 1 : " << result1 << endl;
+    cout << "Bitwise And Result with class 2 : " << result2 << endl;
+    cout << "Bitwise And Result with class 3 : " << result3 << endl;
     return 0;
 }
