3c496494fc2a670562eabfd3af7df3ddac4df717
Aryan-Gupta2003 <aryang.gzb@gmail.com>
2024-02-23 16:39:19 +0530

    Added comments

--- cheapest_flights.cpp
+++ cheapest_flights.cpp
@@ -1,6 +1,19 @@
+// Leetcode problem statement link
+// https://leetcode.com/problems/cheapest-flights-within-k-stops/
+
+// There are n cities connected by some number of flights.
+// You are given an array flights where flights[i] =
+// [fromi, toi, pricei] indicates that there is a flight
+// from city fromi to city toi with cost pricei.
+
+// You are also given three integers src, dst, and k,
+// return the cheapest price from src to dst with at
+// most k stops. If there is no such route, return -1.
+
 #include <bits/stdc++.h>
 using namespace std;
 
+// Sol For Leetcode
 class Solution
 {
 public:
@@ -29,6 +42,7 @@ public:
     }
 };
 
+// For VSCode
 int main()
 {
     Solution o1;
