23eed44cde5dc41d372e4cb69a767b9770fe1434
Aryan-Gupta2003 <aryang.gzb@gmail.com>
2024-02-23 21:35:20 +0530

    Added comments

--- reverse_no.cpp
+++ reverse_no.cpp
@@ -1,6 +1,18 @@
+// Leetcode problem statement link
+// https://leetcode.com/problems/reverse-integer/
+
+// Given a signed 32-bit integer x, return x with
+// its digits reversed. If reversing x causes the
+// value to go outside the signed 32-bit integer
+// range [-231, 231 - 1], then return 0.
+
+// Assume the environment does not allow you to
+// store 64-bit integers (signed or unsigned).
+
 #include <bits/stdc++.h>
 using namespace std;
 
+// Sol For Leetcode
 class Solution
 {
 public:
@@ -23,6 +35,7 @@ public:
     }
 };
 
+// For VSCode
 int main()
 {
     Solution o1;
