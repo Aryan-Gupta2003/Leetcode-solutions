8fb81652f0a3959916e6f584bcd86f29112c9b67
Aryan-Gupta2003 <aryang.gzb@gmail.com>
2024-02-25 15:45:10 +0530

    Added comments

--- str_to_int.cpp
+++ str_to_int.cpp
@@ -1,6 +1,33 @@
+// Leetcode problem statement link
+// https://leetcode.com/problems/string-to-integer-atoi/
+
+// Implement the function, which converts a
+// string to a 32-bit signed integer.
+
+// The algorithm for myAtoi(string s) is as follows:
+
+// 1. Read in and ignore any leading whitespace.
+// 2. Check if the next character (if not already at the end of
+//    the string) is '-' or '+'. Read this character in if it is
+//    either. This determines if the final result is negative or
+//    positive respectively. Assume the result is positive if
+//    neither is present.
+// 3. Read in next the characters until the next non-digit
+//    character or the end of the input is reached. The
+//    rest of the string is ignored.
+// 4. Convert these digits into an integer (i.e. "123" -> 123,
+//    "0032" -> 32). If no digits were read, then the integer is 0.
+//    Change the sign as necessary (from step 2).
+// 5. If the integer is out of the 32-bit signed integer range
+//    [-231, 231 - 1], then clamp the integer so that it remains in the
+//    range. Specifically, integers less than -231 should be clamped to
+//    -231, and integers greater than 231 - 1 should be clamped to 231 - 1.
+// 6. Return the integer as the final result.
+
 #include <bits/stdc++.h>
 using namespace std;
 
+// Sol for Leetcode
 class Solution
 {
 public:
@@ -63,6 +90,7 @@ public:
     }
 };
 
+// For VSCode
 int main()
 {
     Solution o1;
