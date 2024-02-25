90afee309e9d44820ad70a7519c743e9f1e8078d
Aryan-Gupta2003 <aryang.gzb@gmail.com>
2024-02-25 15:57:51 +0530

    Added comments

--- town_judge.cpp
+++ town_judge.cpp
@@ -1,6 +1,26 @@
+// Leetcode problem statement link
+// https://leetcode.com/problems/find-the-town-judge/
+
+// In a town, there are n people labeled from 1 to n. There
+// is a rumor that one of these people is secretly the town judge.
+
+// If the town judge exists, then:
+
+// 1. The town judge trusts nobody.
+// 2. Everybody (except for the town judge) trusts the town judge.
+// 3. There is exactly one person that satisfies properties 1 and 2.
+// 4. You are given an array trust where trust[i] = [ai, bi]
+//    representing that the person labeled ai trusts the
+//    person labeled bi. If a trust relationship does not exist
+//    in trust array, then such a trust relationship does not exist.
+
+// Return the label of the town judge if the town judge exists
+// and can be identified, or return -1 otherwise.
+
 #include <bits/stdc++.h>
 using namespace std;
 
+// Sol for Leetcode
 class Solution
 {
 public:
@@ -23,6 +43,7 @@ public:
     }
 };
 
+// For VSCode
 int main()
 {
     Solution o1;
