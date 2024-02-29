a45d0ba255b9e8ccec705e8f1ffdac7d67709197
Aryan-Gupta2003 <aryang.gzb@gmail.com>
2024-02-29 21:45:47 +0530

    Added comments

--- diameter_of_tree.cpp
+++ diameter_of_tree.cpp
@@ -1,3 +1,16 @@
+// Leetcode problem statement link
+// https://leetcode.com/problems/diameter-of-binary-tree/
+
+// Given the root of a binary tree, return
+// the length of the diameter of the tree.
+
+// The diameter of a binary tree is the length of
+// the longest path between any two nodes in a tree.
+// This path may or may not pass through the root.
+
+// The length of a path between two nodes is
+// represented by the number of edges between them.
+
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -11,6 +24,7 @@ struct TreeNode
     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
 };
 
+// Sol for Leetcode
 class Solution
 {
 public:
@@ -35,6 +49,7 @@ public:
     }
 };
 
+// For VSCode
 int main()
 {
     Solution o1;
