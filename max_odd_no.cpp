00a9b0c7bf4009a138f3a76e1ffc8c1bd740abb0
Aryan-Gupta2003 <aryang.gzb@gmail.com>
2024-03-01 11:46:08 +0530

    Added comments

--- max_odd_no.cpp
+++ max_odd_no.cpp
@@ -1,6 +1,20 @@
+// Leetcode problem statement link
+// https://leetcode.com/problems/maximum-odd-binary-number/
+
+// You are given a binary string s that contains
+// at least one '1'.
+
+// You have to rearrange the bits in such a way that
+// the resulting binary number is the maximum odd
+// binary number that can be created from this combination.
+
+// Return a string representing the maximum odd binary
+// number that can be created from the given combination.
+
 #include <bits/stdc++.h>
 using namespace std;
 
+// Sol For Leetcode
 class Solution
 {
 public:
@@ -27,6 +41,7 @@ public:
     }
 };
 
+// For VSCode
 int main()
 {
     Solution o1;
