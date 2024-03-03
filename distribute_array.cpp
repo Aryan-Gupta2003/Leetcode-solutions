bd67d89e01ae503469f6b54718a60412a770c46c
Aryan-Gupta2003 <aryang.gzb@gmail.com>
2024-03-03 22:37:37 +0530

    Added comments

--- distribute_array.cpp
+++ distribute_array.cpp
@@ -1,6 +1,24 @@
+// Leetcode problem statement link
+// https://leetcode.com/problems/distribute-elements-into-two-arrays-i/
+
+// You are given a 1-indexed array of distinct
+// integers nums of length n.
+
+// You need to distribute all the elements of
+// nums between two arrays arr1 and arr2 using
+// n operations. In the first operation, append
+// nums[1] to arr1. In the second operation,
+// append nums[2] to arr2. Afterwards,
+// in the ith operation:
+
+// If the last element of arr1 is greater than
+// the last element of arr2, append nums[i] to
+// arr1. Otherwise, append nums[i] to arr2.
+
 #include <bits/stdc++.h>
 using namespace std;
 
+// Sol For Leetcode
 class Solution
 {
 public:
@@ -32,6 +50,7 @@ public:
     }
 };
 
+// For VSCode
 int main()
 {
     Solution o1;
