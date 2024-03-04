d1f16794017db156296a0b02909e28f0450fa7a8
Aryan-Gupta2003 <aryang.gzb@gmail.com>
2024-03-04 18:14:53 +0530

    Added comments

--- tokens_bag.cpp
+++ tokens_bag.cpp
@@ -1,6 +1,29 @@
+// Leetcode problem statement link
+// https://leetcode.com/problems/bag-of-tokens/
+
+// You start with an initial power of power,
+// an initial score of 0, and a bag of tokens
+// given as an integer array tokens, where
+// each tokens[i] donates the value of tokeni.
+
+// Your goal is to maximize the total score by
+// strategically playing these tokens. In one
+// move, you can play an unplayed token in one
+// of the two ways (but not both for the same token):
+
+// 1. Face-up: If your current power is at least
+//    tokens[i], you may play tokeni, losing
+//    tokens[i] power and gaining 1 score.
+// 2. Face-down: If your current score is at least
+//    1, you may play tokeni, gaining tokens[i]
+//    power and losing 1 score.
+
+// Return the maximum possible score you can achieve after playing any number of tokens.
+
 #include <bits/stdc++.h>
 using namespace std;
 
+// Sol For Leetcode
 class Solution
 {
 public:
@@ -35,6 +58,7 @@ public:
     }
 };
 
+// For VSCode
 int main()
 {
     Solution o1;
