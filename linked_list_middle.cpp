9665e1c2b7304407afd07ea8c9ee363413381a42
Aryan-Gupta2003 <aryang.gzb@gmail.com>
2024-03-07 18:39:35 +0530

    Add new file

---
+++ linked_list_middle.cpp
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct ListNode
+{
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+class Solution
+{
+public:
+    int length(ListNode *head)
+    {
+        vector<int> arr;
+        ListNode *p = head;
+        while (p != nullptr)
+        {
+            arr.push_back(p->val);
+            p = p->next;
+        }
+        return int(arr.size() / 2);
+    }
+    ListNode *middleNode(ListNode *head)
+    {
+        int l = length(head);
+        ListNode *q = head;
+        while (l--)
+        {
+            q = q->next;
+        }
+        return q;
+    }
+};
+
+int main()
+{
+    Solution o1;
+    ListNode *head = new ListNode(1);
+    head->next = new ListNode(2);
+    head->next->next = new ListNode(3);
+    head->next->next->next = new ListNode(4);
+    head->next->next->next->next = new ListNode(5);
+
+    ListNode *r = o1.middleNode(head);
+    cout << "List from middle node to the end: ";
+    while (r != nullptr)
+    {
+        cout << r->val << " ";
+        r = r->next;
+    }
+    cout << endl;
+
+    return 0;
+}
