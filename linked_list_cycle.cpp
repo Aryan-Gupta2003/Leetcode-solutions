b711652dc82cc9b1a257b1bac060a5e691128083
Aryan-Gupta2003 <aryang.gzb@gmail.com>
2024-03-09 12:10:11 +0530

    Added comments

--- linked_list_cycle.cpp
+++ linked_list_cycle.cpp
@@ -1,3 +1,19 @@
+// Leetcode problem statement link
+// https://leetcode.com/problems/linked-list-cycle/
+
+// Given head, the head of a linked list, determine
+// if the linked list has a cycle in it.
+
+// There is a cycle in a linked list if there is
+// some node in the list that can be reached again
+// by continuously following the next pointer.
+// Internally, pos is used to denote the index of
+// the node that tail's next pointer is connected to.
+// Note that pos is not passed as a parameter.
+
+// Return true if there is a cycle in the linked
+// list. Otherwise, return false.
+
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -8,6 +24,7 @@ struct ListNode
     ListNode(int x) : val(x), next(NULL) {}
 };
 
+// Sol For Leetcode
 class Solution
 {
 public:
@@ -29,6 +46,7 @@ public:
     }
 };
 
+// For VSCode
 int main()
 {
     Solution o1;
