1d3993d23160de7ea1b17e105897363574ccafff
Aryan-Gupta2003 <aryang.gzb@gmail.com>
2024-03-11 19:03:18 +0530

    Added comments

--- custom_sort_str.cpp
+++ custom_sort_str.cpp
@@ -1,6 +1,24 @@
+// Leetcode problem statement link
+// https://leetcode.com/problems/apple-redistribution-into-boxes/
+
+// You are given an array apple of size n and an
+// array capacity of size m.
+
+// There are n packs where the ith pack contains
+// apple[i] apples. There are m boxes as well, and
+// the ith box has a capacity of capacity[i] apples.
+
+// Return the minimum number of boxes you need to
+// select to redistribute these n packs of apples
+// into boxes.
+
+// Note that, apples from the same pack can be
+// distributed into different boxes.
+
 #include <bits/stdc++.h>
 using namespace std;
 
+// Sol For Leetcode
 class Solution
 {
 public:
@@ -25,6 +43,7 @@ public:
     }
 };
 
+// For VSCode
 int main()
 {
     Solution o1;
