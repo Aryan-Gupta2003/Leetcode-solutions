7d0458abe6b1177e8dce931a01c1375a420f83a3
Aryan-Gupta2003 <aryang.gzb@gmail.com>
2024-03-13 23:05:24 +0530

    Added comments

--- find_pivot_int.cpp
+++ find_pivot_int.cpp
@@ -1,6 +1,21 @@
+// Leetcode problem statement link
+// https://leetcode.com/problems/find-the-pivot-integer/
+
+// Given a positive integer n, find the pivot
+// integer x such that:
+
+// The sum of all elements between 1 and x
+// inclusively equals the sum of all elements
+// between x and n inclusively.
+// Return the pivot integer x. If no such
+// integer exists, return -1. It is guaranteed
+// that there will be at most one pivot index
+// for the given input.
+
 #include <bits/stdc++.h>
 using namespace std;
 
+// Sol For Leetcode
 class Solution
 {
 public:
@@ -27,6 +42,7 @@ public:
     }
 };
 
+// For VSCode
 int main()
 {
     Solution o1;
