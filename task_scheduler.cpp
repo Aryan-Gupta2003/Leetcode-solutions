52cd74f2fe2dfe43527ff9056032e652c10d4a93
Aryan-Gupta2003 <aryang.gzb@gmail.com>
2024-03-20 21:36:46 +0530

    Added comments

--- task_scheduler.cpp
+++ task_scheduler.cpp
@@ -1,6 +1,20 @@
+// Leetcode problem statement link
+// https://leetcode.com/problems/task-scheduler/
+
+// You are given an array of CPU tasks, each represented
+// by letters A to Z, and a cooling time, n. Each cycle
+// or interval allows the completion of one task. Tasks
+// can be completed in any order, but there's a constraint:
+// identical tasks must be separated by at least n
+// intervals due to cooling time.
+
+// ​Return the minimum number of intervals required
+// to complete all tasks.
+
 #include <bits/stdc++.h>
 using namespace std;
 
+// Sol For leetcode
 class Solution
 {
 public:
@@ -27,6 +41,7 @@ public:
     }
 };
 
+// For VSCode
 int main()
 {
     Solution o1;
