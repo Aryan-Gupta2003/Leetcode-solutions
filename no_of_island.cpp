7a3f1db1b28b991bf0ce87c389e8c3d2edbde927
Aryan-Gupta2003 <aryang.gzb@gmail.com>
2024-04-19 22:09:41 +0530

    Add new file

---
+++ no_of_island.cpp
@@ -0,0 +1,59 @@
+// https://leetcode.com/problems/number-of-islands/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    Solution()
+    {
+        ios_base::sync_with_stdio(false);
+        cin.tie(NULL);
+        cout.tie(NULL);
+    }
+    void dfs(vector<vector<char>> &grid, int i, int j)
+    {
+        if (i < 0 || i >= grid.size() || j < 0 || j >= grid[0].size() || grid[i][j] != '1')
+        {
+            return;
+        }
+
+        grid[i][j] = '0';
+        dfs(grid, i + 1, j);
+        dfs(grid, i - 1, j);
+        dfs(grid, i, j + 1);
+        dfs(grid, i, j - 1);
+    }
+
+    int numIslands(vector<vector<char>> &grid)
+    {
+        if (grid.empty() || grid[0].empty())
+        {
+            return 0;
+        }
+
+        int numIslands = 0;
+        for (int i = 0; i < grid.size(); i++)
+        {
+            for (int j = 0; j < grid[0].size(); j++)
+            {
+                if (grid[i][j] == '1')
+                {
+                    numIslands++;
+                    dfs(grid, i, j);
+                }
+            }
+        }
+        return numIslands;
+    }
+};
+
+int main()
+{
+    Solution o1;
+    vector<vector<char>> grid{{'1', '1', '1', '1', '0'}, {'1', '1', '0', '1', '0'}, {'1', '1', '0', '0', '0'}, {'0', '0', '0', '0', '0'}};
+    int result = o1.numIslands(grid);
+    cout << "No. of Islands : " << result << endl;
+    return 0;
+}
