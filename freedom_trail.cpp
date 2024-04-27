374a5757ef9e06169246a4569ec480a0680ed10d
Aryan-Gupta2003 <aryang.gzb@gmail.com>
2024-04-27 18:05:18 +0530

    Add new file

---
+++ freedom_trail.cpp
@@ -0,0 +1,62 @@
+// https://leetcode.com/problems/freedom-trail/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    Solution()
+    {
+        ios_base::sync_with_stdio(false);
+        cin.tie(NULL);
+        cout.tie(NULL);
+    }
+    int findRotateSteps(string ring, string key)
+    {
+        unordered_map<char, vector<int>> positions;
+        for (int i = 0; i < ring.size(); i++)
+        {
+            positions[ring[i]].push_back(i);
+        }
+        vector<vector<int>> memo(key.size(), vector<int>(ring.size(), -1));
+        return helper(0, 0, positions, key, ring, memo);
+    }
+    int helper(int in_index, int pos, unordered_map<char, vector<int>> &positions, string &key, string &ring, vector<vector<int>> &memo)
+    {
+        if (in_index == key.size())
+        {
+            return 0;
+        }
+        if (memo[in_index][pos] != -1)
+        {
+            return memo[in_index][pos];
+        }
+        int min_steps = INT_MAX;
+        for (int i : positions[key[in_index]])
+        {
+            int steps;
+            if (i >= pos)
+            {
+                steps = min(i - pos, static_cast<int>(pos + ring.size()) - i);
+            }
+            else
+            {
+                steps = min(pos - i, static_cast<int>(i + ring.size()) - pos);
+            }
+            int next_steps = helper(in_index + 1, i, positions, key, ring, memo);
+            min_steps = min(min_steps, steps + next_steps);
+        }
+        memo[in_index][pos] = min_steps + 1;
+        return min_steps + 1;
+    }
+};
+
+int main()
+{
+    Solution o1;
+    string ring = "godding", key = "gd";
+    int result = o1.findRotateSteps(ring, key);
+    cout << result << endl;
+    return 0;
+}
