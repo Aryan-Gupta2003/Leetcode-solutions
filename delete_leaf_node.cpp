6974a9a246ea2636a7310bce55eae708f6822cd6
Aryan-Gupta2003 <aryang.gzb@gmail.com>
2024-05-17 17:43:13 +0530

    Add new file

---
+++ delete_leaf_node.cpp
@@ -0,0 +1,82 @@
+// https://leetcode.com/problems/delete-leaves-with-a-given-value/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution
+{
+public:
+    Solution()
+    {
+        ios_base::sync_with_stdio(false);
+        cin.tie(NULL);
+        cout.tie(NULL);
+    }
+    TreeNode *removeLeafNodes(TreeNode *root, int target)
+    {
+        if (root == nullptr)
+            return nullptr;
+        root->left = removeLeafNodes(root->left, target);
+        root->right = removeLeafNodes(root->right, target);
+        if (!root->left && !root->right && root->val == target)
+            return nullptr;
+        return root;
+    }
+};
+
+void printTree(TreeNode *root)
+{
+    if (!root)
+    {
+        cout << "null";
+        return;
+    }
+    queue<TreeNode *> q;
+    q.push(root);
+    while (!q.empty())
+    {
+        TreeNode *node = q.front();
+        q.pop();
+        if (node)
+        {
+            cout << node->val << " ";
+            q.push(node->left);
+            q.push(node->right);
+        }
+        else
+        {
+            cout << "null ";
+        }
+    }
+    cout << endl;
+}
+
+int main()
+{
+    Solution o1;
+    TreeNode *root = new TreeNode(1);
+    root->left = new TreeNode(2, new TreeNode(2), new TreeNode(4));
+    root->right = new TreeNode(3, new TreeNode(2), new TreeNode(5));
+
+    cout << "Original tree (in-order): ";
+    printTree(root);
+    cout << endl;
+
+    int target = 2;
+    root = o1.removeLeafNodes(root, target);
+
+    cout << "Tree after removing leaf nodes with value " << target << " (in-order): ";
+    printTree(root);
+    cout << endl;
+    return 0;
+}
