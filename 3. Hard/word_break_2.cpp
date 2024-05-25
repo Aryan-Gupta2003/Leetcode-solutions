c51df27fc01c773839209550095904910edc80b1
Aryan-Gupta2003 <aryang.gzb@gmail.com>
2024-05-25 23:12:01 +0530

    Add new file

---
+++ 3. Hard/word_break_2.cpp
@@ -0,0 +1,81 @@
+// https://leetcode.com/problems/word-break-ii/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    vector<string> ans;
+
+    Solution()
+    {
+        ios_base::sync_with_stdio(false);
+        cin.tie(NULL);
+        cout.tie(NULL);
+    }
+
+    void solve(int i, string &sentence, string &givenString, unordered_map<string, bool> &doesExist)
+    {
+        if (i >= givenString.size())
+        {
+            ans.push_back(sentence);
+            return;
+        }
+        for (int j = i + 1; j <= i + 10 && j <= givenString.size(); j++)
+        {
+            string word = givenString.substr(i, j - i);
+            if (doesExist.find(word) != doesExist.end())
+            {
+                if (sentence.size() == 0)
+                {
+                    sentence = word;
+                }
+                else
+                {
+                    sentence += " " + word;
+                }
+                solve(j, sentence, givenString, doesExist);
+                int x = 0;
+                while (x < word.size())
+                {
+                    x++;
+                    sentence.pop_back();
+                }
+                if (sentence.size() > 0)
+                {
+                    sentence.pop_back();
+                }
+            }
+        }
+    }
+
+    vector<string> wordBreak(string s, vector<string> &wordDict)
+    {
+        string sentence = "";
+        unordered_map<string, bool> doesExist;
+        for (auto x : wordDict)
+        {
+            doesExist[x] = true;
+        }
+        ans.clear();
+        solve(0, sentence, s, doesExist);
+        return ans;
+    }
+};
+
+int main()
+{
+    Solution o1;
+    string s = "catsanddog", sep = "";
+    vector<string> wordDict{"cat", "cats", "and", "sand", "dog"};
+    vector<string> result = o1.wordBreak(s, wordDict);
+    cout << "Valid Sentences : [";
+    for (auto x : result)
+    {
+        cout << sep << "'" << x << "'";
+        sep = ", ";
+    }
+    cout << "]" << endl;
+    return 0;
+}
