006f476a9b97853ce340ba9d4f907dc9cdc89c69
Aryan-Gupta2003 <aryang.gzb@gmail.com>
2024-05-25 23:30:29 +0530

    Add new file

---
+++ 2. Medium/word_break.cpp
@@ -0,0 +1,57 @@
+// https://leetcode.com/problems/word-break/
+
+#include <bits/stdc++.h>
+#include <unordered_set>
+using namespace std;
+
+class Solution
+{
+public:
+    Solution()
+    {
+        ios_base::sync_with_stdio(false);
+        cin.tie(NULL);
+        cout.tie(NULL);
+    }
+
+    int getMaxLength(const vector<string> &wordDict)
+    {
+        return max_element(begin(wordDict), end(wordDict), [](const auto &a, const auto &b)
+                           { return a.length() < b.length(); })
+            ->length();
+    }
+
+    bool wordBreak(string s, vector<string> &wordDict)
+    {
+        const int n = s.length();
+        const int maxLength = getMaxLength(wordDict);
+        const unordered_set<string> wordSet{begin(wordDict), end(wordDict)};
+        vector<int> dp(n + 1);
+        dp[0] = true;
+
+        for (int i = 1; i <= n; ++i)
+        {
+            for (int j = i - 1; j >= 0; --j)
+            {
+                if (i - j > maxLength)
+                    break;
+                if (dp[j] && wordSet.count(s.substr(j, i - j)))
+                {
+                    dp[i] = true;
+                    break;
+                }
+            }
+        }
+        return dp[n];
+    }
+};
+
+int main()
+{
+    Solution o1;
+    string s = "catsanddog", sep = "";
+    vector<string> wordDict{"cat", "cats", "and", "sand", "dog"};
+    bool result = o1.wordBreak(s, wordDict);
+    cout << "Valid Sentence(s) : " << (result == 1 ? "True" : "False");
+    return 0;
+}
