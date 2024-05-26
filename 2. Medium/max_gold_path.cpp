5d74435c63218b42b05000d9b644e0f09f76eddd
Aryan-Gupta2003 <aryang.gzb@gmail.com>
2024-05-26 23:06:37 +0530

    Added comments

--- 2. Medium/max_gold_path.cpp
+++ 2. Medium/max_gold_path.cpp
@@ -1,8 +1,26 @@
 // https://leetcode.com/problems/path-with-maximum-gold/
 
+// In a gold mine grid of size m x n,
+// each cell in this mine has an integer
+// representing the amount of gold in
+// that cell, 0 if it is empty.
+
+// Return the maximum amount of gold
+// you can collect under the conditions:
+
+// Every time you are located in a cell
+// you will collect all the gold in that cell.
+// From your position, you can walk one step
+// to the left, right, up, or down. You
+// can't visit the same cell more than once.
+// Never visit a cell with 0 gold. You
+// can start and stop collecting gold from
+// any position in the grid that has some gold.
+
 #include <bits/stdc++.h>
 using namespace std;
 
+// Sol For Leetcode
 class Solution
 {
 public:
@@ -50,6 +68,7 @@ public:
     }
 };
 
+// For VSCode
 int main()
 {
     Solution o1;
