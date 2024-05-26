7659a2258dc9be7f35a8ed1380bf34aa6ea3cbb6
Aryan-Gupta2003 <aryang.gzb@gmail.com>
2024-05-26 22:47:59 +0530

    Added comments

--- 2. Medium/min_coin_moves.cpp
+++ 2. Medium/min_coin_moves.cpp
@@ -1,5 +1,19 @@
 // https://leetcode.com/problems/distribute-coins-in-binary-tree/
 
+// You are given the root of a binary
+// tree with n nodes where each node in
+// the tree has node.val coins. There are
+// n coins in total throughout the whole tree.
+
+// In one move, we may choose two adjacent
+// nodes and move one coin from one node to
+// another. A move may be from parent to
+// child, or from child to parent.
+
+// Return the minimum number of moves
+// required to make every node have
+// exactly one coin.
+
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -13,6 +27,7 @@ struct TreeNode
     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
 };
 
+// Sol for Leetcode
 class Solution
 {
 public:
@@ -41,6 +56,7 @@ public:
     }
 };
 
+// For VSCode
 int main()
 {
     Solution o1;
