5c1fc724613802c619a1740c8e2a26c662f3d78a
Aryan-Gupta2003 <aryang.gzb@gmail.com>
2024-05-27 18:21:23 +0530

    Added comments

--- 1. Easy/special_arr_x.cpp
+++ 1. Easy/special_arr_x.cpp
@@ -1,9 +1,21 @@
 // Leetcode problem statement link
 // https://leetcode.com/problems/special-array-with-x-elements-greater-than-or-equal-x/
 
+// You are given an array nums of non-negative
+// integers. nums is considered special if there
+// exists a number x such that there are exactly x
+// numbers in nums that are greater than or equal to x.
+
+// Notice that x does not have to be an element in nums.
+
+// Return x if the array is special, otherwise,
+// return -1. It can be proven that if nums is
+// special, the value for x is unique.
+
 #include <bits/stdc++.h>
 using namespace std;
 
+// Sol For Leetcode
 class Solution
 {
 public:
@@ -41,6 +53,7 @@ public:
     }
 };
 
+// For VSCode
 int main()
 {
     Solution o1;
