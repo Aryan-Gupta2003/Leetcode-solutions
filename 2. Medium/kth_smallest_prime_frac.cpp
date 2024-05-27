21a508ba6d559a8c2ebdcc42ebbbb1ac6799cc5d
Aryan-Gupta2003 <aryang.gzb@gmail.com>
2024-05-27 12:19:13 +0530

    Added comments

--- 2. Medium/kth_smallest_prime_frac.cpp
+++ 2. Medium/kth_smallest_prime_frac.cpp
@@ -1,8 +1,23 @@
+// Leetcode problem statement link
 // https://leetcode.com/problems/k-th-smallest-prime-fraction/
+
+// You are given a sorted integer array arr
+// containing 1 and prime numbers, where all
+// the integers of arr are unique. You are
+// also given an integer k.
+
+// For every i and j where 0 <= i < j < arr.length,
+// we consider the fraction arr[i] / arr[j].
+
+// Return the kth smallest fraction considered.
+// Return your answer as an array of integers
+// of size 2, where answer[0] == arr[i] and
+// answer[1] == arr[j].
 
 #include <bits/stdc++.h>
 using namespace std;
 
+// Sol For Leetcode
 class Solution
 {
 public:
@@ -55,6 +70,7 @@ public:
     }
 };
 
+// For VSCode
 int main()
 {
     Solution o1;
