0f10b97574c44089296106dac108fb17fa74a4b6
Aryan-Gupta2003 <aryang.gzb@gmail.com>
2024-05-27 11:57:03 +0530

    Added comments

--- 3. Hard/worker_min_cost.cpp
+++ 3. Hard/worker_min_cost.cpp
@@ -1,8 +1,31 @@
+// Leetcode problem statement link
 // https://leetcode.com/problems/minimum-cost-to-hire-k-workers/
+
+// There are n workers. You are given two integer
+// arrays quality and wage where quality[i] is the
+// quality of the ith worker and wage[i] is the
+// minimum wage expectation for the ith worker.
+
+// We want to hire exactly k workers to form a
+// paid group. To hire a group of k workers, we
+// must pay them according to the following rules:
+
+// Every worker in the paid group must be paid at
+// least their minimum wage expectation. In the
+// group, each worker's pay must be directly
+// proportional to their quality. This means if a
+// worker’s quality is double that of another worker
+// in the group, then they must be paid twice as much
+// as the other worker.
+// Given the integer k, return the least amount of
+// money needed to form a paid group satisfying the
+// above conditions. Answers within 10-5 of the
+// actual answer will be accepted.
 
 #include <bits/stdc++.h>
 using namespace std;
 
+// Sol For Leetcode
 class Solution
 {
 public:
@@ -48,6 +71,7 @@ public:
     }
 };
 
+// For VSCode
 int main()
 {
     Solution o1;
