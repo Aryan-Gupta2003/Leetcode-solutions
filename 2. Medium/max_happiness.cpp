796a1cc021d375ab6b2ef0b189c1dca07bf13587
Aryan-Gupta2003 <aryang.gzb@gmail.com>
2024-05-27 12:33:32 +0530

    Added comments

--- 2. Medium/max_happiness.cpp
+++ 2. Medium/max_happiness.cpp
@@ -1,8 +1,27 @@
+// Leetcode problem statement link
 // https://leetcode.com/problems/maximize-happiness-of-selected-children/
+
+// You are given an array happiness of length n,
+// and a positive integer k.
+
+// There are n children standing in a queue, where
+// the ith child has happiness value happiness[i].
+// You want to select k children from these n children
+// in k turns.
+
+// In each turn, when you select a child, the happiness
+// value of all the children that have not been selected
+// till now decreases by 1. Note that the happiness value
+// cannot become negative and gets decremented only if it
+// is positive.
+
+// Return the maximum sum of the happiness values of the
+// selected children you can achieve by selecting k children.
 
 #include <bits/stdc++.h>
 using namespace std;
 
+// Sol For Leetcode
 class Solution
 {
 public:
@@ -27,6 +46,7 @@ public:
     }
 };
 
+// For VSCode
 int main()
 {
     Solution o1;
