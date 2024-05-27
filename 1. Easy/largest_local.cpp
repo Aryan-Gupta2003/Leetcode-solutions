212c9b3fb5cd5fa5aa1d1ea9aa2aa1aeb6e8f746
Aryan-Gupta2003 <aryang.gzb@gmail.com>
2024-05-27 11:16:24 +0530

    Added comments

--- 1. Easy/largest_local.cpp
+++ 1. Easy/largest_local.cpp
@@ -1,8 +1,23 @@
+// Leetcode problem statement link
 // https://leetcode.com/problems/largest-local-values-in-a-matrix/
+
+// You are given an n x n integer matrix grid.
+
+// Generate an integer matrix maxLocal of size
+// (n - 2) x (n - 2) such that:
+
+// maxLocal[i][j] is equal to the largest value
+// of the 3 x 3 matrix in grid centered around
+// row i + 1 and column j + 1.
+// In other words, we want to find the largest
+// value in every contiguous 3 x 3 matrix in grid.
+
+// Return the generated matrix.
 
 #include <bits/stdc++.h>
 using namespace std;
 
+// Sol For Leetcode
 class Solution
 {
 public:
@@ -36,6 +51,7 @@ public:
     }
 };
 
+// For VSCode
 int main()
 {
     Solution o1;
