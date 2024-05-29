d5fdfbae4c4b7648b928ea2d22b00b88cde2099b
Aryan-Gupta2003 <aryang.gzb@gmail.com>
2024-05-29 22:22:30 +0530

    Added comments

--- 2. Medium/equal_substr_budget.cpp
+++ 2. Medium/equal_substr_budget.cpp
@@ -1,9 +1,27 @@
 // Leetcode problem statement link
 // https://leetcode.com/problems/get-equal-substrings-within-budget/
 
+// You are given two strings s and t of
+// the same length and an integer maxCost.
+
+// You want to change s to t. Changing
+// the ith character of s to ith character
+// of t costs |s[i] - t[i]| (i.e., the
+// absolute difference between the ASCII
+// values of the characters).
+
+// Return the maximum length of a substring
+// of s that can be changed to be the same
+// as the corresponding substring of t with
+// a cost less than or equal to maxCost. If
+// there is no substring from s that can be
+// changed to its corresponding substring
+// from t, return 0.
+
 #include <bits/stdc++.h>
 using namespace std;
 
+// Sol FOr Leetcode
 class Solution
 {
 public:
@@ -33,6 +51,7 @@ public:
     }
 };
 
+// For VSCode
 int main()
 {
     Solution o1;
