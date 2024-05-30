dfc5665c43e84ad1ec7ff3a2397ae0c0d818a502
Aryan-Gupta2003 <aryang.gzb@gmail.com>
2024-05-30 14:26:07 +0530

    Added comments

--- 2. Medium/count_triplets.cpp
+++ 2. Medium/count_triplets.cpp
@@ -1,9 +1,24 @@
 // Leetcode problem statement link
 // https://leetcode.com/problems/count-triplets-that-can-form-two-arrays-of-equal-xor/
 
+// Given an array of integers arr.
+
+// We want to select three indices i, j and
+// k where (0 <= i < j <= k < arr.length).
+
+// Let's define a and b as follows:
+
+// a = arr[i] ^ arr[i + 1] ^ ... ^ arr[j - 1]
+// b = arr[j] ^ arr[j + 1] ^ ... ^ arr[k]
+// Note that ^ denotes the bitwise-xor operation.
+
+// Return the number of triplets
+// (i, j and k) Where a == b.
+
 #include <bits/stdc++.h>
 using namespace std;
 
+// Sol For Leetcode
 class Solution
 {
 public:
@@ -36,6 +51,7 @@ public:
     }
 };
 
+// For VSCode
 int main()
 {
     Solution o1;
