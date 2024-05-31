5bf0399b95980f1585466584ec4b8aea4e43bff6
Aryan-Gupta2003 <aryang.gzb@gmail.com>
2024-05-31 17:35:32 +0530

    Added comments

--- 2. Medium/single_no_3.cpp
+++ 2. Medium/single_no_3.cpp
@@ -1,9 +1,21 @@
 // Leetcode problem statement link
 // https://leetcode.com/problems/single-number-iii/description/
 
+// Given an integer array nums, in which
+// exactly two elements appear only once
+// and all the other elements appear
+// exactly twice. Find the two elements
+// that appear only once. You can return
+// the answer in any order.
+
+// You must write an algorithm that runs
+// in linear runtime complexity and uses
+// only constant extra space.
+
 #include <bits/stdc++.h>
 using namespace std;
 
+// Sol For Leetcode
 class Solution
 {
 public:
@@ -31,6 +43,7 @@ public:
     }
 };
 
+// For VSCode
 int main()
 {
     Solution o1;
