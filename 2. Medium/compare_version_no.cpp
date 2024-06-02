3cf31a0aa00d37a07c3b4e6eed3148023cac3b9e
Aryan-Gupta2003 <aryang.gzb@gmail.com>
2024-06-02 21:10:04 +0530

    Added comments

--- 2. Medium/compare_version_no.cpp
+++ 2. Medium/compare_version_no.cpp
@@ -1,8 +1,27 @@
+// Leetcode problem statement link
 // https://leetcode.com/problems/compare-version-numbers/
+
+// Given two version strings, version1 and
+// version2, compare them. A version string
+// consists of revisions separated by dots
+// '.'. The value of the revision is its
+// integer conversion ignoring leading zeros.
+
+// To compare version strings, compare their
+// revision values in left-to-right order.
+// If one of the version strings has fewer
+// revisions, treat the missing revision
+// values as 0.
+
+// Return the following:
+// If version1 < version2, return -1.
+// If version1 > version2, return 1.
+// Otherwise, return 0.
 
 #include <bits/stdc++.h>
 using namespace std;
 
+// Sol For Leetcode
 class Solution
 {
 public:
@@ -37,6 +56,7 @@ public:
     }
 };
 
+// For VSCode
 int main()
 {
     Solution o1;
