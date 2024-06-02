39430e322f09f7c9989a924c84766c78431f6f2b
Aryan-Gupta2003 <aryang.gzb@gmail.com>
2024-06-02 22:12:34 +0530

    Added comments

--- 3. Hard/sum_of_dist_in_tree.cpp
+++ 3. Hard/sum_of_dist_in_tree.cpp
@@ -1,8 +1,23 @@
+// Leetcode problem statement link
 // https://leetcode.com/problems/sum-of-distances-in-tree/
+
+// There is an undirected connected tree with n
+// nodes labeled from 0 to n - 1 and n - 1 edges.
+
+// You are given the integer n and the array
+// edges where edges[i] = [ai, bi] indicates
+// that there is an edge between nodes ai and
+// bi in the tree.
+
+// Return an array answer of length n where
+// answer[i] is the sum of the distances
+// between the ith node in the tree and all
+// other nodes.
 
 #include <bits/stdc++.h>
 using namespace std;
 
+// Sol For Leetcode
 class Solution
 {
 private:
@@ -63,6 +78,7 @@ public:
     }
 };
 
+// For VSCode
 int main()
 {
     Solution o1;
