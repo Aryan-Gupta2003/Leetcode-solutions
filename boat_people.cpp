5b133125196136ba7a173f087989c0394ac7fe89
Aryan-Gupta2003 <aryang.gzb@gmail.com>
2024-06-02 21:02:46 +0530

    Added comments

--- boat_people.cpp
+++ boat_people.cpp
@@ -1,8 +1,20 @@
+// Leetcode problem statement link
 // https://leetcode.com/problems/boats-to-save-people/
+
+// You are given an array people where people[i]
+// is the weight of the ith person, and an infinite
+// number of boats where each boat can carry a
+// maximum weight of limit. Each boat carries at
+// most two people at the same time, provided the
+// sum of the weight of those people is at most limit.
+
+// Return the minimum number of boats to carry
+// every given person.
 
 #include <bits/stdc++.h>
 using namespace std;
 
+// Sol For Leetcode
 class Solution
 {
 public:
@@ -39,6 +51,7 @@ public:
     }
 };
 
+// For VSCode
 int main()
 {
     Solution o1;
