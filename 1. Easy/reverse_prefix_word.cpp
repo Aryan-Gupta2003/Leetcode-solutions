b80a0717ebab09a52ff52379fe25cbda2b7a13a2
Aryan-Gupta2003 <aryang.gzb@gmail.com>
2024-06-02 21:22:03 +0530

    Added comments

--- 1. Easy/reverse_prefix_word.cpp
+++ 1. Easy/reverse_prefix_word.cpp
@@ -1,8 +1,22 @@
+// Leetcode problem statement link
 // https://leetcode.com/problems/reverse-prefix-of-word/
+
+// Given a 0-indexed string word and a character
+// ch, reverse the segment of word that starts at
+// index 0 and ends at the index of the first
+// occurrence of ch (inclusive). If the character
+// ch does not exist in word, do nothing.
+
+// For example, if word = "abcdefd" and ch = "d",
+// then you should reverse the segment that starts
+// at 0 and ends at 3 (inclusive). The resulting
+// string will be "dcbaefd".
+// Return the resulting string.
 
 #include <bits/stdc++.h>
 using namespace std;
 
+// Sol For Leetcode
 class Solution
 {
 public:
@@ -38,6 +52,7 @@ public:
     }
 };
 
+// For VSCode
 int main()
 {
     Solution o1;
