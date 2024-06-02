7ff25585a9adc0d6532a49c2b5c3e146d63a09a1
Aryan-Gupta2003 <aryang.gzb@gmail.com>
2024-06-02 21:51:06 +0530

    Added comments

--- 2. Medium/min_xor_operation.cpp
+++ 2. Medium/min_xor_operation.cpp
@@ -1,8 +1,27 @@
+// Leetcode problem statement link
 // https://leetcode.com/problems/minimum-number-of-operations-to-make-array-xor-equal-to-k/
+
+// You are given a 0-indexed integer array nums
+// and a positive integer k.
+
+// You can apply the following operation on the
+// array any number of times:
+// Choose any element of the array and flip a bit
+// in its binary representation. Flipping a bit
+// means changing a 0 to 1 or vice versa.
+// Return the minimum number of operations required
+// to make the bitwise XOR of all elements of the
+// final array equal to k.
+
+// Note that you can flip leading zero bits in the
+// binary representation of elements. For example,
+// for the number (101)2 you can flip the fourth
+// bit and obtain (1101)2.
 
 #include <bits/stdc++.h>
 using namespace std;
 
+// Sol For Leetcode
 class Solution
 {
 public:
@@ -33,6 +52,7 @@ public:
     }
 };
 
+// For VSCode
 int main()
 {
     Solution o1;
