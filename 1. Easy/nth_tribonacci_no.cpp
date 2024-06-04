2a47ad14211838fca0cc3a45d629dc35f97907bd
Aryan-Gupta2003 <aryang.gzb@gmail.com>
2024-06-04 21:36:58 +0530

    Added comments

--- 1. Easy/nth_tribonacci_no.cpp
+++ 1. Easy/nth_tribonacci_no.cpp
@@ -1,8 +1,14 @@
+// Leetcode problem statement link
 // https://leetcode.com/problems/n-th-tribonacci-number/
+
+// The Tribonacci sequence Tn is defined as follows:
+// T0 = 0, T1 = 1, T2 = 1, and Tn+3 = Tn + Tn+1 + Tn+2 for n >= 0.
+// Given n, return the value of Tn.
 
 #include <bits/stdc++.h>
 using namespace std;
 
+// Sol For Leetcode
 class Solution
 {
 public:
@@ -31,6 +37,7 @@ public:
     }
 };
 
+// For VSCode
 int main()
 {
     Solution o1;
