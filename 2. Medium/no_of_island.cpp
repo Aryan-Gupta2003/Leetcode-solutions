3540cb6b420bcf3094bf3ad58cd9100324ec9226
Aryan-Gupta2003 <aryang.gzb@gmail.com>
2024-06-04 22:11:18 +0530

    Added comments

--- 2. Medium/no_of_island.cpp
+++ 2. Medium/no_of_island.cpp
@@ -1,8 +1,20 @@
+// Leetcode problem statement link
 // https://leetcode.com/problems/number-of-islands/
+
+// Given an m x n 2D binary grid grid which
+// represents a map of '1's (land) and '0's
+// (water), return the number of islands.
+
+// An island is surrounded by water and is
+// formed by connecting adjacent lands
+// horizontally or vertically. You may
+// assume all four edges of the grid are
+// all surrounded by water.
 
 #include <bits/stdc++.h>
 using namespace std;
 
+// Sol For Leetcode
 class Solution
 {
 public:
@@ -49,6 +61,7 @@ public:
     }
 };
 
+// For VSCode
 int main()
 {
     Solution o1;
