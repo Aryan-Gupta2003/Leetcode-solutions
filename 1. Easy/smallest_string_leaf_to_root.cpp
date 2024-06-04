2dea6fd5b260aa992ce8097e541a941704173971
Aryan-Gupta2003 <aryang.gzb@gmail.com>
2024-06-04 22:23:13 +0530

    Added comments

--- 1. Easy/smallest_string_leaf_to_root.cpp
+++ 1. Easy/smallest_string_leaf_to_root.cpp
@@ -1,4 +1,20 @@
+// Leetcode problem statement link
 // https://leetcode.com/problems/smallest-string-starting-from-leaf/
+
+// You are given row x col grid representing a
+// map where grid[i][j] = 1 represents land and
+// grid[i][j] = 0 represents water.
+
+// Grid cells are connected horizontally/vertically
+// (not diagonally). The grid is completely surrounded
+// by water, and there is exactly one island (i.e.,
+// one or more connected land cells).
+
+// The island doesn't have "lakes", meaning the water
+// inside isn't connected to the water around the island.
+// One cell is a square with side length 1. The grid is
+// rectangular, width and height don't exceed 100.
+// Determine the perimeter of the island.
 
 #include <bits/stdc++.h>
 using namespace std;
@@ -13,6 +29,7 @@ struct TreeNode
     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
 };
 
+// Sol For Leetcode
 class Solution
 {
 public:
@@ -51,6 +68,7 @@ public:
     }
 };
 
+// For VSCode
 int main()
 {
     Solution o1;
