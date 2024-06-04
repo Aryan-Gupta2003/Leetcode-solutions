c537c0097afa02f73a06d3522754ebd2dfbe7729
Aryan-Gupta2003 <aryang.gzb@gmail.com>
2024-06-04 22:17:07 +0530

    Added comments

--- 1. Easy/island_perimeter.cpp
+++ 1. Easy/island_perimeter.cpp
@@ -1,8 +1,27 @@
+// Leetcode problem statement link
 // https://leetcode.com/problems/island-perimeter/
+
+// You are given row x col grid representing
+// a map where grid[i][j] = 1 represents land
+// and grid[i][j] = 0 represents water.
+
+// Grid cells are connected horizontally/
+// vertically (not diagonally). The grid
+// is completely surrounded by water, and
+// there is exactly one island (i.e., one
+// or more connected land cells).
+
+// The island doesn't have "lakes", meaning
+// the water inside isn't connected to the
+// water around the island. One cell is a
+// square with side length 1. The grid is
+// rectangular, width and height don't exceed
+// 100. Determine the perimeter of the island.
 
 #include <bits/stdc++.h>
 using namespace std;
 
+// Sol For Leetcode
 class Solution
 {
 public:
@@ -40,6 +59,7 @@ public:
     }
 };
 
+// For VSCode
 int main()
 {
     Solution o1;
