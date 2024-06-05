3597f337249d6352417c88635c0c0f62593cf27e
Aryan-Gupta2003 <aryang.gzb@gmail.com>
2024-06-05 14:02:21 +0530

    Add new file

---
+++ 1. Easy/first_common_char.cpp
@@ -0,0 +1,71 @@
+// Leetcode problem statement link
+// https://leetcode.com/problems/find-common-characters/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+private:
+    vector<int> count(const string &str)
+    {
+        vector<int> freq(26, 0);
+        for (char c : str)
+        {
+            freq[c - 'a']++;
+        }
+        return freq;
+    }
+
+    vector<int> intersection(const vector<int> &a, const vector<int> &b)
+    {
+        vector<int> t(26, 0);
+        for (int i = 0; i < 26; i++)
+        {
+            t[i] = min(a[i], b[i]);
+        }
+        return t;
+    }
+
+public:
+    Solution()
+    {
+        ios_base::sync_with_stdio(false);
+        cin.tie(NULL);
+        cout.tie(NULL);
+    }
+    vector<string> commonChars(vector<string> &words)
+    {
+        vector<int> last = count(words[0]);
+        for (int i = 1; i < words.size(); i++)
+        {
+            last = intersection(last, count(words[i]));
+        }
+        vector<string> s;
+        for (int i = 0; i < 26; i++)
+        {
+            while (last[i] > 0)
+            {
+                s.push_back(string(1, 'a' + i));
+                last[i]--;
+            }
+        }
+        return s;
+    }
+};
+
+int main()
+{
+    Solution o1;
+    vector<string> words{"bella", "label", "roller"};
+    string sep = "";
+    vector<string> result = o1.commonChars(words);
+    cout << "Common characters : [";
+    for (string x : result)
+    {
+        cout << sep << "'" << x << "'";
+        sep = ", ";
+    }
+    cout << "]" << endl;
+    return 0;
+}
