ad94530f56d28a7c66d73542d45c9d5b6c5977b4
Aryan-Gupta2003 <aryang.gzb@gmail.com>
2024-06-06 19:35:44 +0530

    Added comments

--- 2. Medium/hand_of_straight.cpp
+++ 2. Medium/hand_of_straight.cpp
@@ -1,9 +1,20 @@
 // Leetcode problem statement link
 // https://leetcode.com/problems/hand-of-straights/
 
+// Alice has some number of cards and she wants to
+// rearrange the cards into groups so that each
+// group is of size groupSize, and consists of
+// groupSize consecutive cards.
+
+// Given an integer array hand where hand[i] is the
+// value written on the ith card and an integer
+// groupSize, return true if she can rearrange the
+// cards, or false otherwise.
+
 #include <bits/stdc++.h>
 using namespace std;
 
+// Sol For Leetcode
 class Solution
 {
 public:
@@ -46,6 +57,7 @@ public:
     }
 };
 
+// For VSCode
 int main()
 {
     Solution o1;
