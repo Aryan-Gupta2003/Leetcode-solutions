e52a1455b81c0c8c3b221ca6ea0918b51d77124e
Aryan-Gupta2003 <aryang.gzb@gmail.com>
2024-06-06 22:43:15 +0530

    Added comments

--- 2. Medium/reveal_cards.cpp
+++ 2. Medium/reveal_cards.cpp
@@ -1,8 +1,28 @@
+// Leetcode problem statement link
 // https://leetcode.com/problems/reveal-cards-in-increasing-order/
+
+// You are given an integer array deck. There is
+// a deck ofcards where every card has a unique
+// integer. The integer on the ith card is deck[i].
+
+// You can order the deck in any order you want. Initially,
+// all the cards start face down (unrevealed) in one deck.
+
+// You will do the following steps repeatedly
+// until all cards are revealed:
+// Take the top card of the deck, reveal it,
+// and take it out of the deck.
+// If there are still cards in the deck then put the
+// next top card of the deck at the bottom of the deck.
+// If there are still unrevealed cards, go back to
+// step 1. Otherwise, stop.
+// Return an ordering of the deck that
+// would reveal the cards in increasing order.
 
 #include <bits/stdc++.h>
 using namespace std;
 
+// Sol For Leetcode
 class Solution
 {
 public:
@@ -39,6 +59,7 @@ public:
     }
 };
 
+// For VSCode
 int main()
 {
     Solution o1;
