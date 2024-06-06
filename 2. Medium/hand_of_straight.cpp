01ecd2bc7950b8d57c27333ae1e08ae09e299094
Aryan-Gupta2003 <aryang.gzb@gmail.com>
2024-06-06 19:31:20 +0530

    Add new file

---
+++ 2. Medium/hand_of_straight.cpp
@@ -0,0 +1,57 @@
+// Leetcode problem statement link
+// https://leetcode.com/problems/hand-of-straights/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    Solution()
+    {
+        ios_base::sync_with_stdio(false);
+        cin.tie(NULL);
+        cout.tie(NULL);
+    }
+    bool isNStraightHand(vector<int> &hand, int groupSize)
+    {
+        if (hand.size() % groupSize != 0)
+            return false;
+
+        unordered_map<int, int> count;
+        for (int card : hand)
+            count[card]++;
+
+        vector<int> sortedKeys;
+        for (auto &pair : count)
+            sortedKeys.push_back(pair.first);
+
+        sort(sortedKeys.begin(), sortedKeys.end());
+        for (int key : sortedKeys)
+        {
+            if (count[key] > 0)
+            {
+                int startCount = count[key];
+                for (int i = key; i < key + groupSize; i++)
+                {
+                    if (count[i] < startCount)
+                    {
+                        return false;
+                    }
+                    count[i] -= startCount;
+                }
+            }
+        }
+        return true;
+    }
+};
+
+int main()
+{
+    Solution o1;
+    vector<int> hand{1, 2, 3, 6, 2, 3, 4, 7, 8};
+    int groupSize = 3;
+    bool result = o1.isNStraightHand(hand, groupSize);
+    cout << "Rearranged Cards : " << (result == 1 ? "True" : "False");
+    return 0;
+}
