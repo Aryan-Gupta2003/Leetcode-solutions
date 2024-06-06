971ea0ad99398547d0431754be77a443b6284a4e
Aryan-Gupta2003 <aryang.gzb@gmail.com>
2024-06-06 19:41:42 +0530

    Added comments

--- root_to_leaf_sum.cpp
+++ root_to_leaf_sum.cpp
@@ -1,4 +1,13 @@
+// Leetcode problem statement link
 // https://leetcode.com/problems/sum-root-to-leaf-numbers/
+
+// You are given the root of a binary tree containing digits from 0 to 9 only.
+// Each root-to-leaf path in the tree represents a number.
+
+// For example, the root-to-leaf path 1 -> 2 -> 3 represents the number
+// 123. Return the total sum of all root-to-leaf numbers. Test cases
+// are generated so that the answer will fit in a 32-bit integer.
+// A leaf node is a node with no children.
 
 #include <bits/stdc++.h>
 using namespace std;
@@ -13,6 +22,7 @@ struct TreeNode
     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
 };
 
+// Sol For Leetcode
 class Solution
 {
 public:
@@ -43,6 +53,7 @@ public:
     }
 };
 
+// For VSCode
 int main()
 {
     Solution o1;
